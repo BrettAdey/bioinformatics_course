856307bf358ba2514d96f81b4f46c02bf0403270
Ubuntu <ubuntu@ip-172-31-37-104.eu-west-2.compute.internal>
2021-03-11 15:42:37 +0000

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+# make empty directories in your git repo
+mkdir -p analysis docs data
+# add a README.md to each directory
+# the scripts directory already exists
+for my_directory in scripts analysis docs data;do
+  touch ${my_directory}/README.md
+  echo "# ${my_directory}" >> ${my_directory}/README.md
+done
